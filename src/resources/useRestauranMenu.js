bbd4d1f3163600881a39d6560cf6ec09bf34c75c
shreekantkerkar <shreekantkerkar85@gmail.com>
2024-08-22 17:24:16 +0530

    final changes before deployment

--- src/resources/useRestauranMenu.js
+++ src/resources/useRestauranMenu.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { MENU_API } from "./constants";
 
 const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState(null);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     const data = await fetch(MENU_API + resId);
     const json = await data.json();
     setResInfo(json);
-  };
+  }, [resId]);  // Add resId as a dependency if it's used inside fetchData
 
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
+  }, [fetchData]);  // Now fetchData is a stable dependency
 
   return resInfo;
 };
